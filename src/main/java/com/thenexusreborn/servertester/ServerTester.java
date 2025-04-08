1f3b5180c7d18c2446b23c454fc2f7006644357d
Firestar311 <slaterandrew0398@gmail.com>
2025-04-07 20:56:23 -0500

    Initial Commit

---
+++ src/main/java/com/thenexusreborn/servertester/ServerTester.java
@@ -0,0 +1,139 @@
+package com.thenexusreborn.servertester;
+
+import java.io.*;
+import java.lang.ProcessBuilder.Redirect;
+import java.nio.file.*;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Stream;
+
+public class ServerTester {
+    public static void main(String[] args) {
+        Path propertiesFile = Path.of("tester.properties");
+        if (!Files.exists(propertiesFile)) {
+            try {
+                Files.createDirectories(propertiesFile.toAbsolutePath().getParent());
+            } catch (IOException e) {
+                System.err.println("Could not create directories for the tester.properties file.");
+                return;
+            }
+            
+            try (InputStream resourceStream = ServerTester.class.getResourceAsStream("tester.properties")) {
+                if (resourceStream == null) {
+                    System.err.println("Could not find a default tester.properties file. This is a bug");
+                    return;
+                }
+                
+                Files.copy(resourceStream, propertiesFile, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("Created a default tester.properties file. Please change as needed");
+            } catch (IOException e) {
+                System.err.println("Error while copying the default properties file");
+                e.printStackTrace();
+            }
+        }
+        
+        Properties properties = new Properties();
+        try {
+            properties.load(Files.newInputStream(propertiesFile));
+            System.out.println("Loaded settings from the tester.properties file");
+        } catch (IOException e) {
+            System.err.println("Could not load properties from the tester.properties file");
+            e.printStackTrace();
+            return;
+        }
+        
+        Path serverDir = Path.of(properties.getProperty("serverDir").replace("{sep}", File.separator).replace("{user.home}", System.getProperty("user.home")));
+        Path nexusCoreDir = Path.of(properties.getProperty("nexusCoreDir").replace("{sep}", File.separator).replace("{user.home}", System.getProperty("user.home")));
+        Path nexusHubDir = Path.of(properties.getProperty("nexusHubDir").replace("{sep}", File.separator).replace("{user.home}", System.getProperty("user.home")));
+        Path nexusMapsDir = Path.of(properties.getProperty("nexusMapsDir").replace("{sep}", File.separator).replace("{user.home}", System.getProperty("user.home")));
+        Path nexusSGDir = Path.of(properties.getProperty("nexusSGDir").replace("{sep}", File.separator).replace("{user.home}", System.getProperty("user.home")));
+        Path starChatDir = Path.of(properties.getProperty("starChatDir").replace("{sep}", File.separator).replace("{user.home}", System.getProperty("user.home")));
+        Path starCoreDir = Path.of(properties.getProperty("starCoreDir").replace("{sep}", File.separator).replace("{user.home}", System.getProperty("user.home")));
+        Path starItemsDir = Path.of(properties.getProperty("starItemsDir").replace("{sep}", File.separator).replace("{user.home}", System.getProperty("user.home")));
+        
+        Path pluginsDir = FileSystems.getDefault().getPath(serverDir.toAbsolutePath().toString(), "plugins");
+        Path oldNexusCoreFile = getPluginFile(pluginsDir, "NexusCore");
+        Path oldNexusHubFile = getPluginFile(pluginsDir, "NexusHub");
+        Path oldNexusMapsFile = getPluginFile(pluginsDir, "NexusMaps");
+        Path oldNexusSGFile = getPluginFile(pluginsDir, "NexusSurvivalGames");
+        Path oldStarChatFile = getPluginFile(pluginsDir, "StarChat");
+        Path oldStarCoreFile = getPluginFile(pluginsDir, "StarCore");
+        Path oldStarItemsFile = getPluginFile(pluginsDir, "StarItems");
+        
+        try {
+            deleteIfExists(oldNexusCoreFile);
+            deleteIfExists(oldNexusHubFile);
+            deleteIfExists(oldNexusMapsFile);
+            deleteIfExists(oldNexusSGFile);
+            deleteIfExists(oldStarChatFile);
+            deleteIfExists(oldStarCoreFile);
+            deleteIfExists(oldStarItemsFile);
+        } catch (Exception e) {
+            System.err.println("Error while deleting the old files");
+            e.printStackTrace();
+            return;
+        }
+        
+        Path newNexusCoreFile = getPluginFile(nexusCoreDir, "NexusCore");
+        Path newNexusHubFile = getPluginFile(nexusHubDir, "NexusHub");
+        Path newNexusMapsFile = getPluginFile(nexusMapsDir, "NexusMaps");
+        Path newNexusSGFile = getPluginFile(nexusSGDir, "NexusSurvivalGames");
+        Path newStarChatFile = getPluginFile(starChatDir, "StarChat");
+        Path newStarCoreFile = getPluginFile(starCoreDir, "StarCore");
+        Path newStarItemsFile = getPluginFile(starItemsDir, "StarItems");
+        
+        try {
+            Files.copy(newNexusCoreFile, newPluginPath(pluginsDir, newNexusCoreFile));
+            Files.copy(newNexusHubFile, newPluginPath(pluginsDir, newNexusHubFile));
+            Files.copy(newNexusMapsFile, newPluginPath(pluginsDir, newNexusMapsFile));
+            Files.copy(newNexusSGFile, newPluginPath(pluginsDir, newNexusSGFile));
+            Files.copy(newStarChatFile, newPluginPath(pluginsDir, newStarChatFile));
+            Files.copy(newStarCoreFile, newPluginPath(pluginsDir, newStarCoreFile));
+            Files.copy(newStarItemsFile, newPluginPath(pluginsDir, newStarItemsFile));
+        } catch (Exception e) {
+            System.err.println("Error while copying new files");
+            e.printStackTrace();
+            return;
+        }
+        
+        String serverJarName = properties.getProperty("serverJarFile");
+        ProcessBuilder processBuilder = new ProcessBuilder("java", "-Xmx4G", "-jar", serverJarName, "nogui");
+        processBuilder.directory(serverDir.toFile());
+        processBuilder.redirectOutput(Redirect.INHERIT);
+        processBuilder.redirectError(Redirect.INHERIT);
+        processBuilder.redirectInput(Redirect.INHERIT);
+        try {
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+            System.out.println("Server exited with exit code: " + exitCode);
+        } catch (Exception e) {
+            System.err.println("Error while trying to run the server");
+            e.printStackTrace();
+        }
+    }
+    
+    public static void deleteIfExists(Path path) throws IOException {
+        if (path == null) {
+            return;
+        }
+        
+        Files.deleteIfExists(path);
+    }
+    
+    public static Path newPluginPath(Path destDir, Path file) {
+        return FileSystems.getDefault().getPath(destDir.toAbsolutePath().toString(), file.getFileName().toString());
+    }
+    
+    public static Path getPluginFile(Path dir, String name) {
+        try (Stream<Path> stream = Files.walk(dir)) {
+            List<Path> results = stream.filter(p -> !Files.isDirectory(p)).filter(p -> p.getFileName().toString().startsWith(name)).toList();
+            if (!results.isEmpty()) {
+                return results.getFirst();
+            }
+        } catch (IOException e) {
+            System.err.println("Error while trying to find the plugin file " + name + " in " + dir.toAbsolutePath());
+        }
+        
+        return null;
+    }
+}
